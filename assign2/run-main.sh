c0a3de24af18076ea697d05df7e3e39b9095005a
hengruo <hengruoz@gmail.com>
2018-11-19 12:22:16 -0500

    run scripts

--- assign2/run-main.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp assign_parsing.jar:assign_parsing-submit.jar -server -mx2000m edu.berkeley.nlp.assignments.parsing.PCFGParserTester -path ./data -parserType GENERATIVE -maxTestLength 20
--- assign2/run-sc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -cp assign_parsing.jar:assign_parsing-submit.jar -server -mx300m edu.berkeley.nlp.assignments.parsing.PCFGParserTester -path ./data -parserType GENERATIVE -sanityCheck
